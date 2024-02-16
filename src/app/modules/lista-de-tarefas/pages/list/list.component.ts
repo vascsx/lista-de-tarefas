adbf96ad9f93e43de4b73bd9f9f35e94c825fbb8
vascsx <vasczn@gmail.com>
2024-02-15 23:29:09 -0300

    feat: add input-add-item component

--- src/app/modules/lista-de-tarefas/pages/list/list.component.ts
+++ src/app/modules/lista-de-tarefas/pages/list/list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
+import { IListItems } from '../../interface/IListItems.interface';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [InputAddItemComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  public addItem = signal(true);
 
+  #setListItesms = signal<IListItems[]>([this.#parseItems()]);
+  getListItems = this.#setListItesms.asReadonly();
+
+  #parseItems (){
+    return JSON.parse(localStorage.getItem('@my-list') || '[]');
+  }
+  public getInputAndAddItem (value : IListItems) {
+    localStorage.setItem(
+      '@mylist', JSON.stringify([value]));
+  }
 }
